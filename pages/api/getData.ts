401c6a8cc77c489d4ec34457c253c6681969a1b1
yoggsoft <yoggsoft@gmail.com>
2021-11-16 20:51:01 +0100

    initial commit

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import TimeSlots from '../../../time_slots.json';
+import { Company } from '../../types';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Array<Company>>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(TimeSlots)
 }
---
+++ types/index.ts
@@ -0,0 +1,12 @@
+
+export type TimeSlot = {
+	start_time: string,
+	end_time: string
+}
+
+export type Company = {
+	id: number,
+	name: string,
+	type: string,
+	time_slots: Array<TimeSlot>
+}
